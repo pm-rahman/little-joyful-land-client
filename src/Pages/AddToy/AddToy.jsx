e9b9dc50737d9396f42a919a5a15f457a7e2e47b
pm-rahman <p.m.rahaman0181@gmail.comm>
2023-05-18 22:54:09 +0600

    create toy in data base

--- src/Pages/AddToy/AddToy.jsx
+++ src/Pages/AddToy/AddToy.jsx
@@ -1,15 +1,49 @@
 import { useContext } from "react";
 import { useForm } from "react-hook-form";
 import { AuthContext } from "../../Providers/AuthProvider/AuthProvider";
+import Title from "../../Components/Title/Title";
+import Swal from "sweetalert2";
 const AddToy = () => {
     const { user } = useContext(AuthContext)
     const { register, handleSubmit, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
+    Title('Add-Toy')
+
+    const onSubmit = data => {
+        console.log(data);
+        fetch('http://localhost:5000/toys', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.acknowledged) {
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: 'Toy Created Successfully',
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                }
+                console.log(data);
+            })
+    };
     return (
         <div>
             <h3 className="text-3xl text-center font-bold mb-4">Add New Toy</h3>
             <form onSubmit={handleSubmit(onSubmit)} className="grid grid-cols-3 gap-x-10 gap-y-4 bg-blue-100 p-12 rounded-lg">
                 {/* register your input into the hook by invoking the "register" function */}
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text text-lg font-semibold">Toy Picture</span>
+                    </label>
+                    <label className="input-group">
+                        <input type="url" {...register("toyPic", { required: true })} placeholder="Toy Picture" className="input input-bordered w-full" />
+                    </label>
+                </div>
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text text-lg font-semibold">Toy Name</span>
@@ -39,7 +73,7 @@ const AddToy = () => {
                         <span className="label-text text-lg font-semibold">Sub Category </span>
                     </label>
                     <select {...register("subCategory")} className="select select-primary w-full">
-                    <option value="sportsCar">Sports car</option>
+                        <option value="sportsCar">Sports car</option>
                         <option value="truck">Truck</option>
                         <option value="regularCar">Regular car</option>
                     </select>
@@ -73,7 +107,7 @@ const AddToy = () => {
                         <span className="label-text text-lg font-semibold">Detail description</span>
                     </label>
                     <label className="input-group">
-                        <input type="text" {...register("description", { required: true })} placeholder="Detail description" className="input input-bordered w-full" />
+                        <textarea type="text" {...register("description", { required: true })} placeholder="Detail description" className="textarea w-full" ></textarea>
                     </label>
                 </div>
                 {errors.exampleRequired && <span className="text-red-500 font-semibold">This field is required</span>}
