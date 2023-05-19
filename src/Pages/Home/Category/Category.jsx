1e8b8c99669cf2c39578b9b19f6558486b2092e0
pm-rahman <p.m.rahaman0181@gmail.comm>
2023-05-19 12:49:42 +0600

    hightlight product section done

--- src/Pages/Home/Category/Category.jsx
+++ src/Pages/Home/Category/Category.jsx
@@ -1,59 +1,89 @@
+import { useEffect, useState } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 
 const Category = () => {
+    const [category, setCategory] = useState('sportsCar')
+    const [toys, setToys] = useState([])
+    useEffect(() => {
+        fetch(`http://localhost:5000/toy-category?category=${category}`)
+            .then(res => res.json())
+            .then(data => setToys(data))
+    }, [category])
     return (
         <Tabs>
             <TabList>
-                <Tab><span className='font-semibold'>Sports car</span></Tab>
-                <Tab><span className='font-semibold'>Truck</span></Tab>
-                <Tab><span className='font-semibold'>Regular car</span></Tab>
+                <Tab onClick={() => setCategory('sportsCar')}><span className='font-semibold'>Sports car</span></Tab>
+                <Tab onClick={() => setCategory('truck')}><span className='font-semibold'>Truck</span></Tab>
+                <Tab onClick={() => setCategory('regularCar')}><span className='font-semibold'>Regular car</span></Tab>
             </TabList>
 
             <TabPanel>
                 <div className='grid grid-cols-3 gap-4'>
-                    <div className="card w-96 bg-base-100 shadow-xl">
-                        <img className="rounded-md max-h-full max-w-auto m-auto" src="https://i.ibb.co/d0Rvw01/gallery-pic-3.jpg" alt="" />
-                        <div className="card-body">
-                            <h2 className="card-title">
-                                Name
-                                <div className="badge badge-secondary">Rating</div>
-                            </h2>
-                            <p>Price</p>
-                            <button className="btn bg-blue-600 hover:bg-blue-700 border-0">Buy Now</button>
+                    {
+                        category === 'sportsCar' && toys.map(toy => <div
+                            key={toy._id}
+                            className='grid grid-cols-3 gap-4'>
+                            <div className="card w-96 bg-base-100 shadow-md">
+                                <img className="rounded-md max-h-full max-w-auto m-auto" src={toy.toyPic} alt="" />
+                                <div className="card-body">
+                                    <h2 className="card-title">
+                                        {toy.toyName}
+                                        <div className="badge badge-secondary">{toy.rating}</div>
+                                    </h2>
+                                    <p>${toy.price}</p>
+                                    <button className="btn bg-blue-600 hover:bg-blue-700 border-0">Buy Now</button>
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                        )
+                    }
                 </div>
             </TabPanel>
             <TabPanel>
                 <div className='grid grid-cols-3 gap-4'>
-                    <div className="card w-96 bg-base-100 shadow-xl">
-                    <img className="rounded-md max-h-full max-w-full m-auto" src="https://m.media-amazon.com/images/I/71O6ZBfa7TL.jpg" alt="" />
-                        <div className="card-body">
-                            <h2 className="card-title">
-                                Name
-                                <div className="badge badge-secondary">Rating</div>
-                            </h2>
-                            <p>Price</p>
-                            <button className="btn bg-blue-600 hover:bg-blue-700 border-0">Buy Now</button>
+                    {
+                        category === 'truck' && toys.map(toy => <div
+                            key={toy._id}
+                            className='grid grid-cols-3 gap-4'>
+                            <div className="card w-96 bg-base-100 shadow-md">
+                                <img className="rounded-md max-h-full max-w-auto m-auto" src={toy.toyPic} alt="" />
+                                <div className="card-body">
+                                    <h2 className="card-title">
+                                        {toy.toyName}
+                                        <div className="badge badge-secondary">{toy.rating}</div>
+                                    </h2>
+                                    <p>${toy.price}</p>
+                                    <button className="btn bg-blue-600 hover:bg-blue-700 border-0">Buy Now</button>
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                        )
+                    }
                 </div>
             </TabPanel>
             <TabPanel>
                 <div className='grid grid-cols-3 gap-4'>
-                    <div className="card w-96 bg-base-100 shadow-xl">
-                    <img className="rounded-md max-h-full max-w-full m-auto" src="https://i.ibb.co/QFDsfhP/gallery-pic-2.png" alt="" />
-                        <div className="card-body">
-                            <h2 className="card-title">
-                                Name
-                                <div className="badge badge-secondary">Rating</div>
-                            </h2>
-                            <p>Price</p>
-                            <button className="btn bg-blue-600 hover:bg-blue-700 border-0">Buy Now</button>
+                    {
+                        category === 'regularCar' && toys.map(toy => <div
+                            key={toy._id}
+                            className='grid grid-cols-3 gap-4'>
+                            <div className="card w-96 bg-base-100 shadow-md">
+                                <img className="rounded-md max-h-full max-w-auto m-auto" src={toy.toyPic} alt="" />
+                                <div className="card-body">
+                                    <h2 className="card-title">
+                                        {toy.toyName}
+                                        <div className="badge badge-secondary">{toy.rating}</div>
+                                    </h2>
+                                    <p>${toy.price}</p>
+                                    <button className="btn bg-blue-600 hover:bg-blue-700 border-0">Buy Now</button>
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                        )
+                    }
                 </div>
+
             </TabPanel>
         </Tabs>
     );
--- src/Pages/Home/HighlightProduct/HighlightProduct.jsx
+++ src/Pages/Home/HighlightProduct/HighlightProduct.jsx
@@ -1,25 +1,40 @@
+import { useEffect, useState } from "react";
+
 const HighlightProduct = () => {
+    const [highlightToys, setHighlightToys] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:5000/highlightProduct')
+            .then(res => res.json())
+            .then(data => setHighlightToys(data))
+    }, [])
     return (
         <div className="grid md:grid-cols-2 gap-6">
             <div>
                 <h3 className="mb-6 border-l-4 pl-5 border-l-blue-500 text-lg sm:text-xl md:text-2xl font-bold">New Products</h3>
-                <div className="flex gap-10 items-center px-8 py-3 rounded-lg shadow-md hover:shadow-lg">
-                    <img className="rounded-md h-auto w-28" src="https://i.ibb.co/d0Rvw01/gallery-pic-3.jpg" alt="" />
+                {highlightToys.slice(0, 3).map(toy => <div
+                    key={toy._id}
+                    className="flex gap-10 mb-3 items-center px-8 py-3 rounded-lg shadow-md hover:shadow-lg">
+                    <img className="rounded-md hover:scale-125 hover:duration-300 h-auto w-28" src={toy.toyPic} alt="" />
                     <div className="text-xl">
-                        <h4 className="font-bold">Name</h4>
-                        <p className="text-blue-600 font-semibold">price</p>
+                        <h4 className="font-bold">{toy.toyName}</h4>
+                        <p className="text-blue-600">${toy.price}</p>
                     </div>
                 </div>
+                )}
+
             </div>
             <div>
                 <h3 className="mb-6 border-l-4 pl-5 border-l-blue-500 text-lg sm:text-xl md:text-2xl font-bold">Best Rated Products</h3>
-                <div className="flex gap-10 items-center px-8 py-3 rounded-lg shadow-md hover:shadow-lg">
-                    <img className="rounded-md h-auto w-28" src="https://i.ibb.co/d0Rvw01/gallery-pic-3.jpg" alt="" />
+                {highlightToys.slice(3, 6).map(toy => <div
+                    key={toy._id}
+                    className="flex gap-10 mb-3 items-center px-8 py-3 rounded-lg shadow-md hover:shadow-lg">
+                    <img className="rounded-md hover:scale-125 hover:duration-300 h-auto w-28" src={toy.toyPic} alt="" />
                     <div className="text-xl">
-                        <h4 className="font-bold">Name</h4>
-                        <p className="text-blue-600 font-semibold">price</p>
+                        <h4 className="font-bold">{toy.toyName}</h4>
+                        <p className="text-blue-600">${toy.price}</p>
                     </div>
                 </div>
+                )}
             </div>
         </div>
     );
