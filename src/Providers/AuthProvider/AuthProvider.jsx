9bf85c32fab2aa17e88ca8249f3a03a42ac023fe
pm-rahman <p.m.rahaman0181@gmail.comm>
2023-05-21 13:46:14 +0600

    login related little change

--- src/Providers/AuthProvider/AuthProvider.jsx
+++ src/Providers/AuthProvider/AuthProvider.jsx
@@ -26,7 +26,7 @@ const AuthProvider = ({children}) => {
             setUser(currentUser);
         })
         return unsubscribe;
-    },[])
+    },[user])
     const logOut = ()=>{
         return signOut(auth);
     }
