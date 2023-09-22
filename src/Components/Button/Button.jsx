d2cd98db3f9da72d199a40ecb309374491e2830b
pm-rahman <p.m.rahaman0181@gmail.comm>
2023-09-22 15:40:57 +0600

    modified::buttion,slider

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,6 +1,6 @@
 
 const Button = ({onClick,variant,className,children}) => {
-    return <button onClick={onClick} className={`px-7 py-3 font-semibold
+    return <button onClick={onClick} className={`text-sm sm:text-base px-5 sm:px-7 py-2 sm:py-3 font-semibold
     ${variant?
         variant!=="outline"?
         "":
--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -3,15 +3,15 @@ import banner from "../../../assets/banner.webp";
 const Banner = () => {
   return (
     <div className="overflow-hidden px-3 rounded relative">
-      <img className="h-full w-auto" src={banner} alt="" />
+      <img className="h-auto w-full" src={banner} alt="" />
       <div className="container absolute h-full flex items-center top-0 z-20">
-        <div>
+        <div className="p-2 sm:p-0">
           <p className="text-sm sm:text-lg">Exclusive Products is Here</p>
-          <h4 className="text-xl mt-3 sm:text-5xl font-bold">Make Your Kids</h4>
-          <h4 className="text-lg font-sans mt-2 sm:text-4xl">
+          <h4 className="text-xl mt-1 sm:mt-3 sm:text-5xl font-bold">Make Your Kids</h4>
+          <h4 className="text-lg font-sans mt-1 sm:mt-2 sm:text-4xl">
             Happy & Healthy
           </h4>
-          <Button variant="outline" className="mt-5">
+          <Button variant="outline" className="mt-1 sm:mt-5">
             Shop Now
           </Button>
         </div>
